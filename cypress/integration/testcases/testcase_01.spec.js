365dd604b489607599aa7afdb81804362084bc1a
Khyati Rani <ranikhyati@gmail.com>
2021-12-09 16:18:01 +0530

    first commit

---
+++ cypress/integration/testcases/testcase_01.spec.js
@@ -0,0 +1,49 @@
+import homePage from '../pageObject/homePage'
+describe('Check the Page Navigation',()=>{
+const hp=new homePage()
+
+    before(()=>{
+      cy.fixture('testData').then((data)=>
+      {
+      globalThis.data=data
+      })
+    })
+      before('Visits the URL', () => {
+        hp.visit()
+      
+      })
+      it('Verify Page title',()=>{
+        hp.getPageTitle().should('contains','QA')
+      })
+      it('visits to  the "ABOUT" page',()=>{
+        hp.navigateToAboutPage()
+      })
+
+      it('checks if "About QA InfoTech" is present on that page',()=>{
+        hp.checkContent().contains('About QA InfoTech')
+      })
+    
+      it('visits to "SERVICES" page',()=>{
+        hp.navigatetoServicesPage()
+      })
+      it('checks if "Functional Testing Services Overview" is present on that page',()=>{
+        hp.verifyTheContent().contains('Functional Testing Services Overview')
+      })
+      it('visits to "SERVICES" page',()=>{
+        hp.navigateToQuotePage()
+      })
+      it('Gets the input field and inputs text', () => {
+        hp.entersTheName(data.firstName,data.lastName)
+      })
+      it('Select the location from drop down list', () => {
+        hp.selectFromDropDownList(data.location)
+      })
+      it('checks if "Request For Quote" is present on that page',()=>{
+        hp.checkThecontent().contains('Request For Quote')
+      })
+
+  
+  
+     })
+   
+
