20b6c3b287637080dbedb866b586e6284d8b59f5
Khyati Rani <ranikhyati@gmail.com>
2022-01-18 12:30:45 +0530

    modify circle.ci file

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -29,6 +29,26 @@ function getConfigurationByFile(file) {
 /**
  * @type {Cypress.PluginConfig}
  */
+
+
+ module.exports = (on, config) => {
+  // inside config.browsers array each object has information like
+  // {
+  //   name: 'chrome',
+  //   channel: 'canary',
+  //   family: 'chromium',
+  //   displayName: 'Canary',
+  //   version: '80.0.3966.0',
+  //   path:
+  //    '/Applications/Canary.app/Contents/MacOS/Canary',
+  //   majorVersion: 80
+  // }
+  return {
+    browsers: config.browsers.filter((b) => b.family === 'chromium'),
+  }
+
+
+
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
     console.log(config.env) 
