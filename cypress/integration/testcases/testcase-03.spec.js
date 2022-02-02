2ea96c782d59c43207ca2db1b587cc5fc6a9a5e9
Khyati Rani <ranikhyati@gmail.com>
2022-02-02 08:46:05 +0530

    updatyed split command

--- cypress/integration/testcases/testcase-03.spec.js
+++ cypress/integration/testcases/testcase-03.spec.js
@@ -18,35 +18,107 @@ describe('Check the Page Navigation', () => {
 
   })
 
-  it.skip('should run tests with async/await', async () => {
 
-    /* const Log = await promisify(cy.get('[data-testid="next-btn-login"] > .MuiButton-label'))
+  it('Validate get request', () => {
+  })
 
-    expect(body).to.equal('body')*/
-   
-  }) 
+    it.only('POST-create',async () => {
+      const item = { 'email': 'abc@gmail.com', 'password': 'FuelF@rFuture123' }
+      //const header = {"Content-Type": "application/json"}
+      //cy.request('POST','https://api.nightly.futurefuel.io/api/1/auth/login', item,header)
+      const login = await promisify(cy.api({
+        method: 'POST',
+        url: 'https://api.nightly.futurefuel.io/api/1/auth/login',
+
+        // baseUrl is prepend to URL
+        form: true, // indicates the body should be form urlencoded and sets Content-Type: application/x-www-form-urlencoded headers
+        body: {
+          email: 'abc@gmail.com',
+          password: 'FuelF@rFuture123',
+        },
+        headers: {
+          'Content-Type': 'application/json'
+          // },
+
+          /*   }).its('body')
+            .its('data')
+            .should('include', {}) */
+        },
+      }).then(response => response.login)
+      )
+      
+      // all your assertions should be placed here!!
+      const cookies = login.headers['set-cookie'];
+      // cookies.forEach(cookie => {
+      // parse cookies and do what you need with them
+
+      const lo = await promisify(cy.api({
+        url: 'https://api.nightly.futurefuel.io/api/1/users/banks',
+        headers: {
+          'cookie': cookies
+        },
+
+        body: { totalpost: 5 }
+
+      }).then(response => response.lo)
+      )
+       
+
+        //Write Unique ID to a fixture file
+        cy.writeFile('cypress/fixtures/resData.json', {
+          "response": lo.body
+        
+
+      })
 
 
-  it('should run tests with async/await', async () => {
-    //const body = await promisify(cy.get('body'))
-    //expect(body).to.be.visible
 
-    await promisify(cy.get('notfound', { timeout: 1000 })).catch(err => {
-     expect(err.message).to.contain('Timed out retrying after 1000ms: Expected to find element:')
-    return true
+
+
+
+      it.skip('should run tests with async/await', async () => {
+
+        /* const Log = await promisify(cy.get('[data-testid="next-btn-login"] > .MuiButton-label'))
+    
+        expect(body).to.equal('body')*/
+
+
+      })
+
+
+      it('should run tests with async/await', async () => {
+        //const body = await promisify(cy.get('body'))
+        //expect(body).to.be.visible
+
+        const getData = async () => {
+          var y = await "Hello World";
+          console.log(y);
+        }
+
+        console.log(1);
+        getData();
+        console.log(2);
+
+
+
+        await promisify(cy.get('notfound', { timeout: 1000 })).catch(err => {
+          expect(err.message).to.contain('Timed out retrying after 1000ms: Expected to find element:')
+          return true
+        })
+      })
+
+      it('assert - given value is an object', () => {
+        const employee = {
+          name: 'abc',
+          age: 1,
+        }
+
+        assert.isObject(employee, 'value is object')
+      })
+
     })
+  
   })
-
-  it('assert - given value is an object', () => {
-    const employee = {
-      name: 'abc',
-      age: 1,
-    }
-
-    assert.isObject(employee, 'value is object')
-  })
-
-})
 
 
 
